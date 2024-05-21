6c4fa985b936be74db81356753eb3a4b848b20e4
konimimi <konimimi5233@gmail.com>
2024-05-21 13:48:34 +0900

    Add b2

--- exercise_B/test_B.py
+++ exercise_B/test_B.py
@@ -1,9 +1,13 @@
-def generate_geometric_progression(initial_term,common_ratio,num):
+def generate_geometric_progression(initial_term, common_ratio, num):
     ans = []
     for i in range(num):
         ans.append(initial_term * common_ratio ** i)
     return ans
 
+def calculate_arithmetic_series(initial_term, common_ratio, num):
+    t = sum(generate_geometric_progression(initial_term, common_ratio, num))
+    return t
+
 if __name__ == '__main__':
-    result = generate_geometric_progression(initial_term = 2,common_ratio = 3,num = 10)
+    result = calculate_arithmetic_series(initial_term = 2, common_ratio = 3, num = 10)
     print(result)
